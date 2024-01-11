0dc8660b46e5eac2ea6b428088852230467f5a34
SkollEatsTheSun <n.nasypalov@gmail.com>
2024-01-11 10:36:51 +0300

    Update models

--- mytestprj/advert/api/models.py
+++ mytestprj/advert/api/models.py
@@ -4,82 +4,42 @@ from authentication.models import CustomUser
 
 class Category(models.Model):
 
-    #Модель Категории
     name = models.CharField(max_length=31, unique=True, verbose_name="Название категории")
-
     def __str__(self):
         return str(self.name)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Advert(models.Model):
 
-    #Модель Объявления
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='advert', verbose_name="Автор объявления")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='advert', verbose_name="Категория вещи")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='adverts', verbose_name="Автор объявления")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='adverts', verbose_name="Категория вещи")
     name = models.CharField(max_length=60, verbose_name="Название объявления")
     description = models.TextField(verbose_name="Описание")
     photo = models.ImageField(upload_to='avatars', verbose_name="Фото вещи")
 
     def __str__(self):
-        return str(self.id)
+        return str(self.name)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
 
 class Proposal(models.Model):
 
-    #Модель Заявки
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='proposal', verbose_name="Автор заявки")
-    advert = models.ForeignKey(Advert, on_delete=models.CASCADE,related_name='proposal', verbose_name="Объявление")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='proposals', verbose_name="Автор заявки")
+    advert = models.ForeignKey(Advert, on_delete=models.CASCADE,related_name='proposals', verbose_name="Объявление")
     reward = models.IntegerField(blank=True, null=True, verbose_name="Денежное вознаграждение")
     comment = models.CharField(max_length= 120, blank=True, null=True, verbose_name="Комментарий к заявке")
     accepted = models.BooleanField(default=False, verbose_name="Принята автором объявления")
 
     def __str__(self):
-        return str(self.id)
-
-
-'''
-class Category(models.Model):
-    """
-    Модель Категории
-    """
-    name = models.CharField(max_length=31, unique=True, verbose_name="Название категории")
-
-    def __str__(self):
-        return str(self.name)
+        return str(self.advert)
 
     class Meta:
-        verbose_name_plural = 'Categories'
-
-
-class Advert(models.Model):
-    """
-    Модель Объявления
-    """
-
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Автор объявления")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория вещи")
-    name = models.CharField(max_length=63, verbose_name="Название объявления")
-    description = models.CharField(max_length=127, verbose_name="Описание")
-    photo = models.ImageField(upload_to='avatars', verbose_name="Фото вещи")
-
-    def __str__(self):
-        return str(self.id)
-
-
-class Proposal(models.Model):
-    """
-    Модель Заявки
-    """
-
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Автор заявки")
-    advert = models.ForeignKey(Advert, on_delete=models.CASCADE, verbose_name="Объявление")
-    reward = models.IntegerField(blank=True, null=True, verbose_name="Денежное вознаграждение")
-    comment = models.CharField(max_length=127, blank=True, null=True, verbose_name="Комментарий к заявке")
-    accepted = models.BooleanField(default=False, verbose_name="Принята автором объявления")
-
-    def __str__(self):
-        return str(self.id)
-'''
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
